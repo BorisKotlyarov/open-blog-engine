616dc615b9817dad8c72f2c0250aa1fb630cc102
BorisKolyarov <17835383+BorisKotlyarov@users.noreply.github.com>
2018-08-30 19:43:44 +0300

    config.js has been added

--- backend/apiServer.js
+++ backend/apiServer.js
@@ -1,7 +1,8 @@
 const { Modules, Server }   = require('just-rest');
-const PORT = process.env.PORT || 3003;
+const config                = require('./config');
+
 
 Modules.defineResponseInterceptor('./interceptors/corsAllowHeaders.js');
 Modules.define('./modules/categories/index.js');
 
-new Server({Modules, port:PORT });
+new Server({Modules, port:config.server.port });
---
+++ backend/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    server: {
+        port: process.env.PORT || 3003
+    },
+    database: {
+        url: "mongodb://localhost:27017/open-blog-engine"
+    }
+};
